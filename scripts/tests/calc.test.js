f4405031adab4f7c12e599d5816db216bd25f174
Krnsand <krnsandin@gmail.com>
2023-02-03 10:52:09 +0000

    Write addition function to test, it passed

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,25 @@
+const { TestScheduler } = require("jest");
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20,22)).toBe(42);
+        })
+    })});
+
+    describe("Calculator", () => {
+        describe("Subtraction function", () => {
+    
+    })});
+    
+    describe("Calculator", () => {
+        describe("Multiply function", () => {
+        
+    })});
+        
+    describe("Calculator", () => {
+        describe("Division function", () => {
+        
+        });
+});
